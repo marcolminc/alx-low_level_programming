115fed460b5d1bdc3726cddfd3008f06cc7acfe4
marcolminc <thommarcolminc@gmail.com>
2023-12-30 08:04:27 -0800

    update source code for the program should return failure value(1) and rectify a typo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, msg, sizeof(msg) - 1);
-	return (0);
+	return (1);
 }

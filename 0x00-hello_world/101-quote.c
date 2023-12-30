03d46e3696e8871b41193ebbb6e57f4fee6a6e6a
marcolminc <thommarcolminc@gmail.com>
2023-12-30 07:56:40 -0800

    add c source code for  a program which writes a sentence to stderr without using std lib

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints a quote to stderr
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return (0);
+}

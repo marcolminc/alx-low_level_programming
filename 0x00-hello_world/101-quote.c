d3e956b33cb7399682295f8056e22e9ab7daa48f
marcolminc <thommarcolminc@gmail.com>
2023-12-30 08:01:26 -0800

    update the source code to fix bug of leaving std lib instead of unistd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - prints a quote to stderr
@@ -9,6 +9,7 @@
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-19-19\n";
+
 	write(2, msg, sizeof(msg) - 1);
 	return (0);
 }

fbd743c0359f5ae68cefe03edf62cda7251307aa
marcolminc <thommarcolminc@gmail.com>
2023-12-30 05:50:34 -0800

    add  c program that prints to console using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints a sentence to console
+ * return: always 0
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

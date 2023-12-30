300152568de027cb39ebced9f0f06173995e0bf9
marcolminc <thommarcolminc@gmail.com>
2023-12-30 06:03:47 -0800

    modify the source code for betty to stay happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 
 /**
  * main - prints a sentence to console
- * return: always 0
+ * Return: always 0
  */
-int main()
+
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);

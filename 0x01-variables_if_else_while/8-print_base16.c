cfae6e673d796007ab3ae48b44a2d851e4694e33
marcolminc <thommarcolminc@gmail.com>
2023-12-31 00:41:28 -0800

    create a program that prints digits of base 10 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits of base 16 numbers.
+ *
+ * Return: Always 0(success).
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

ada3cb6b33e21008101fccc02f1a41639c1c537c
marcolminc <thommarcolminc@gmail.com>
2023-12-30 21:25:27 -0800

    create program that print alphabets in both lowercase and uppercase, merged on a singleline

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets, both lower- and upper-case, merged on a line
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

f7d078b131e0996151f14bc54f359d6a835a3f31
marcolminc <thommarcolminc@gmail.com>
2024-01-05 12:04:25 -0800

    create a function that prints and returns the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a given number.
+ * @n: the given number whose last digit should be printed.
+ *
+ * Return: the value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int d;
+
+	d = n % 10;
+	_putchar(d + '0');
+	return (d);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,6 @@ int _islower(int);
 int _isalpha(int);
 int print_sign(int);
 int _abs(int);
+int print_last_digit(int);
 
 #endif

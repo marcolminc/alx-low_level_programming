23dfec97e539825e2de97658f548b986c347c82c
marcolminc <thommarcolminc@gmail.com>
2024-01-05 11:37:35 -0800

    create function that returns absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of a number.
+ * @n: the number whose absolute value should be returned.
+ *
+ * Return: absolute value of n.
+ */
+int _abs(int n);
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}

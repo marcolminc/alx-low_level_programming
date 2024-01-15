0a30b3deec5daa7f528e4b625b219243677c8e39
marcolminc <thommarcolminc@gmail.com>
2024-01-15 05:48:45 -0800

    add _isupper function source code

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the ASCII value of the character to be checked
+ *
+ * Return: 1 if c is upper, 0 utherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}

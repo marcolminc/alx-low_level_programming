7f9dc8f30659cdbfa2de632a620c45b7e9c7e592
marcolminc <thommarcolminc@gmail.com>
2024-01-15 07:21:25 -0800

    create a function that prints a diagonal out of \ chars

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,11 @@
+#include "main.h"
+
 /**
  * print_diagonal - prints a diagonal by using \ char
  * @n: number of times '\' should be printed, forming a diagonal
  * Return: void
  */
- void print_diagonal(int n)
+void print_diagonal(int n)
 {
 	int i, j;
 

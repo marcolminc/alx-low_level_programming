ad353c5fef8948e5c7578fc0b24a9e729da6718c
marcolminc <thommarcolminc@gmail.com>
2024-01-15 07:02:15 -0800

    create a function that prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line by n times '_' char
+ * @n: number of times '_' should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (n)
+		_putchar('_');
+	_putchar('\n');
+}	

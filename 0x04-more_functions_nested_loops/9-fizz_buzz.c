26596c87a3f5e3f8e75c6f0a973a219e1fe9a409
marcolminc <thommarcolminc@gmail.com>
2024-01-23 21:12:56 -0800

    update the function to consider cases when number is both divisible by 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,12 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz" );
+			else
+				printf("Fizz");
+		}
 		else if (i % 5 == 0)
 			_printf("Buzz");
 		else

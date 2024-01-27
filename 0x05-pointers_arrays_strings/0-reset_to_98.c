88ed4e2bd9bc362bccac1f229c34b669d4ed3206
marcolminc <thommarcolminc@gmail.com>
2024-01-27 07:57:48 -0800

    begin new project on pointers, arrays and strings
    tackle the first task, a function that resets value of a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets th value of a variable to 98.
+ * @n: the pointer to a variable whose value shoud be reset
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# OXO5 - POINTERS,ARRAYS AND STRINGS
+	Learning basics and advanced usage of pointers and
+their relationships with arrays and strings.
+
+## table of contents:
+

06ba0144d3d31acd5f395c414eaf480ab9fdda09
marcolminc <thommarcolminc@gmail.com>
2024-01-27 14:07:25 -0800

    create a function that prints a string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * str_len - counts characters in a string
+ * @s: the string
+ * Return: the number of characters
+ */
+int str_len(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*s != '\0')
+		len++, s++;
+	return (len);
+}
+
+/**
+ * print_rev - print a string in reverse, followed by \n
+ * @s: the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = str_len(s);
+	while (len >= 0)
+		_putchar(*(s + len--));
+	_putchar('\n');
+}

b95c0d7365c2aded5dacb63426972310d14e500d
marcolminc <thommarcolminc@gmail.com>
2024-02-01 03:34:35 -0800

    create a function that prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * print_before_main - prints a string before main() is executed
+ *
+ * Return: void
+ */
+void print_before_main(void)
+{
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -31,6 +31,6 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 void _puts(char *str);
-
+void print_before_main(void) __attribute__((constructor));
 
 #endif

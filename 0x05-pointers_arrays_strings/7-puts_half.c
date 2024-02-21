3cfdfe2556b8508b04fdd89d70d70b58211e9cfb
marcolminc <thommarcolminc@gmail.com>
2024-02-21 13:32:39 -0800

    use else for when size is divisible by two

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -30,7 +30,8 @@ void puts_half(char *str)
 	len = (int)_strlen(str);
 	if (len % 2 != 0)
 		n = (len - 1) / 2;
-	n = len / 2;
+	else
+		n = len / 2;
 	for (; n < len; n++)
 		_putchar(*(str + n));
 	_putchar('\n');

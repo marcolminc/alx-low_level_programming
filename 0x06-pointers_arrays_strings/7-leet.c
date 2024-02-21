861f55b67311752437c82b3222fc350eb0d9b2ab
marcolminc <thommarcolminc@gmail.com>
2024-02-21 06:47:11 -0800

    update leet function

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,16 +9,22 @@
  */
 char *leet(char *str)
 {
-	leet_t enc[5] = {'a', 4, 'e', 3, 'o', 0, 't', 7, 'l', 1};
+	leet_t enc[5] = {
+			{'a', 4},
+			{'e', 3},
+			{'o', 0},
+			{'t', 7},
+			{'l', 1}
+	};
 	int i, j;
 
 	i = 0;
 	while (*(str + i) != '\0')
 	{
-		for (i = 0; j < 5; j++)
+		for (j = 0; j < 5; j++)
 		{
 			if (*(str + i) == enc[j].letter
-					|| *(str + i) + 32 == enc[j].letter)
+			|| *(str + i) + 32 == enc[j].letter)
 			{
 				*(str + i) = '0' + enc[j].num;
 				break;

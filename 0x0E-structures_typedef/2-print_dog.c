08b6d6798cc0da7ff0865a49a38a4354352dfd90
marcolminc <thommarcolminc@gmail.com>
2024-03-25 17:28:39 -0700

    write a function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints members of a dog structure
+ * @d: the dog structure
+ * REturn: void
+ */
+void print_dog(struct dog *d)
+{
+	const char *nil = "(nil)";
+
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("%sName: \n", nil);
+	if (!(d->age < 0 || d->age > 30))
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: %s\n", nil);
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", nil);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,7 +21,7 @@ struct dog
 
 /* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 
 #endif /* DOG_H */

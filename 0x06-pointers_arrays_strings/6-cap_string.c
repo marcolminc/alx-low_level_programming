a290312ac90629e93b1219d5eacf5295e2e9a935
marcolminc <thommarcolminc@gmail.com>
2024-08-19 16:44:08 -0700

    update the function by checking of the first char of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,29 +2,41 @@
 
 
 /**
- * cap_string - capitalizes words of a string
- * @str: the string wholse words to capitalize
+ * is_separator - checks if a char is a word separator
+ * @c: the char to check
  *
- * Return: pointer to the (capitalized) string
+ * Return: 1 if separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+		if (c == separators[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to capiltalize
+ *
+ * Return: capitalized string
  */
 char *cap_string(char *str)
 {
-	int i, j;
-	char delim[] = {' ', '\t', '\n', ',', ';',
-		'.', '!', '?', '"', '(', ')', '{', '}'};
+	int i;
 
 	i = 0;
+	if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		*(str + i) = *(str + i) - 32;
 	while (*(str + i) != '\0')
 	{
-		for (j = 0; j < (int)sizeof(delim); j++)
-		{
-			if (*(str + i) == delim[j])
-			{
-				if (*(str + i + 1) >= 'a' && *(str + i + 1) <= 'z')
-					*(str + i + 1) = *(str + i + 1) - 32;
-				break;
-			}
-		}
+		if (is_separator(*(str + i)) &&
+					*(str + i + 1) >= 'a' &&
+					*(str + i + 1) <= 'z')
+			*(str + i + 1) = *(str + i + 1) - 32;
 		i++;
 	}
 	return (str);

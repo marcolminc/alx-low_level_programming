d54e10de17f3bc86dc872c67daef8d67024a5477
marcolminc <thommarcolminc@gmail.com>
2024-08-19 16:14:43 -0700

    correct padding for printables

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -28,7 +28,7 @@ void print_buffer(char *b, int size)
 			if (i + offset < size)
 				printf("%02x", b[offset + i]);
 			else
-				printf(" ");
+				printf("  ");
 			if (i % 2)
 				printf(" ");
 		}

703b6f17af484123bf98de10d2f44fd3ad8681fb
marcolminc <thommarcolminc@gmail.com>
2024-08-23 16:10:53 -0700

    write a function that concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+
+/**
+ * argstostr - concatenates all cmd args into a string
+ * @ac: number of cmd args(size of av)
+ * @av: the commands matrix (2d array)
+ *
+ * Return: the resulting string
+ */
+char *argstostr(int ac, char **av)
+{
+	int str_len, i, j, k;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	str_len = k = 0, str = NULL;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			str_len++;
+		str_len++;
+	}
+	str = malloc((str_len + 1) * sizeof *str);
+	if (str)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (j = 0; av[i][j] != '\0'; j++)
+				str[k++] = av[i][j];
+			str[k++] = '\n';
+		}
+		str[k] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -10,6 +12,7 @@ char *create_array(unsigned int, char);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
 
 
 #endif /* MAIN_H */

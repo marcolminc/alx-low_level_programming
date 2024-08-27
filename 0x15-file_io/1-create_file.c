c24709df4b622586adadce25d0841a922feea173
marcolminc <thommarcolminc@gmail.com>
2024-08-27 13:25:01 -0700

    create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-	if (*text_content == '\0')
+	if (*text_content == '\0' || text_content == NULL)
 	{
 		close(fd);
 		return (1);

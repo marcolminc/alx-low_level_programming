f43170ffa3427761c41a824116aa321722d19144
marcolminc <thommarcolminc@gmail.com>
2024-08-29 13:12:20 -0700

    drop binary search

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,26 +3,23 @@
 /**
  * _sqrt_recursion_helper - recursive binary alogorithm for sqrt_recursion
  * @n: the int whose natureal square root is required
- * @low: lower bound
- * @high: upper bound
+ * @i: the iterative counter
+ *
  * Return: the square root
  */
-int _sqrt_recursion_helper(int n, int low, int high)
+int _sqrt_recursion_helper(int n, int i)
 {
-	int mid, square;
+	int root;
 
-	if (low > high)
+	if (n == 0 || n == 1)
+		return (n);
+	root = i * i;
+	if (root == n)
+		return (i);
+	if (root > n)
 		return (-1);
-	mid = low + (high - low) / 2;
-	square = mid * mid;
-	if (square == n)
-		return (mid);
-	else if (square < n)
-		return (_sqrt_recursion_helper(n, mid + 1, high));
-	else
-		return (_sqrt_recursion_helper(n, low, mid - 1));
+	return (_sqrt_recursion_helper(n, i + 1));
 }
-
 /**
  * _sqrt_recursion - computes natural square root of an int
  * @n: the int whose square root is required
@@ -32,5 +29,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion_helper(n, 0, n));
+	return (_sqrt_recursion_helper(n, 1));
 }

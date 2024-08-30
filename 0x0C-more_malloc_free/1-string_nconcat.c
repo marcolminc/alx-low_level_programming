a7d603c5a2af77f27d769f1ec30da731897b09d6
marcolminc <thommarcolminc@gmail.com>
2024-08-30 10:55:51 -0700

    write a function that concatenates two strings upt to n bytes of the second one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,26 +12,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int  len1, len2;
+	unsigned int len1, len2;
 	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = NULL;
+		s2 = "";
+
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 	if (n >= len2)
 		n = len2;
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-	ptr = malloc((len1 + n + 1) * sizeof(*ptr));
-	if (ptr == NULL)
-		return (NULL);
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		ptr[len1] = s1[len1];
-	for (len2 = 0; len2 < n; len2++)
-		ptr[len1++] = s2[len2];
-	ptr[len1] = '\0';
-	return (ptr);
+	ptr = malloc((len1 + n + 1) * sizeof(char));
+	if (ptr != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			ptr[len1] = s1[len1];
+		for (len2 = 0; len2 < n; len2++)
+			ptr[len1++] = s2[len2];
+		ptr[len1] = '\0';
+		return (ptr);
+	}
+	return (NULL);
 }

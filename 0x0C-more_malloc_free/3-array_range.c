889980fde127e42bb901cd161ff55df9e5684023
marcolminc <thommarcolminc@gmail.com>
2024-08-31 07:13:18 -0700

    write a function that initializes an array from a range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * array_range - creates an array from a range
+ * @min: minimum of the range(inclusive)
+ * @max: maximum of the range(inclusive)
+ *
+ * Return: the array, NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int size, i, *ptr;
+
+	if (min > max)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = min++;
+	return (ptr);
+}

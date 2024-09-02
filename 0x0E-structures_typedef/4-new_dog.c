317a3a3cbe0c3da95e67caaa5ee6abb30dd48253
marcolminc <thommarcolminc@gmail.com>
2024-09-02 15:01:51 -0700

    write a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+
+/**
+ * _strdup - duplicates a string onto a variable
+ * @str: the string to duplicate
+ * Return: the duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	size_t i, str_size;
+
+	if (str == NULL)
+		return (NULL);
+	i = str_size = 0;
+	while (*(str + str_size) != '\0')
+		str_size++;
+	ptr = (char *)malloc(str_size + 1 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (; *(str + i) != '\0'; i++)
+		*(ptr + i) = *(str + i);
+	*(ptr + i) = '\0';
+	return (ptr);
+}
+
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the new dog
+ * @age: age of the new dog
+ * @owner: owner of the new dog
+ *
+ * Return: the new dog, NULL if it fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *dog_name, *dog_owner;
+	dog_t *new_dog;
+
+	dog_name = _strdup(name);
+	if (dog_name == NULL)
+		return (NULL);
+	dog_owner = _strdup(owner);
+	if (dog_owner == NULL)
+		return (NULL);
+	new_dog = malloc(sizeof(*new_dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = dog_name;
+	new_dog->age = age;
+	new_dog->owner = dog_owner;
+	return (new_dog);
+}

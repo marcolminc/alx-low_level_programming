3f51f18caf15fc4b07e708e7a87bcb3096e1d0a9
marcolminc <thommarcolminc@gmail.com>
2024-09-03 07:47:11 -0700

    write a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,9 +19,13 @@ struct dog
 	float age;
 };
 typedef struct dog dog_t;
+
+
 /* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif /* DOG_H */

5458c5927bdc740b696ed1f78f98512fa07b4946
marcolminc <thommarcolminc@gmail.com>
2025-02-21 12:18:19 -0800

    fix INT_MIN bug

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,8 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = ((n < 0) ? n * -1 : n) % 10;
+	last_digit = n % 10;
+	last_digit = (last_digit < 0) ? last_digit * -1 : last_digit;
 	_putchar('0' + last_digit);
 	return (last_digit);
 }

54ac95de8beb35c160c2d2ff047fcd805318e1e4
marcolminc <thommarcolminc@gmail.com>
2025-02-22 08:14:20 -0800

    use uint128_t for much higher accuracy for larger fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 
+
+/**
+ * print_uint128_t - prints uint128_t number
+ * @n: the number to print
+ *
+ * Return: nothing
+ */
+void print_uint128_t(__uint128_t n)
+{
+	if (n > 9)
+		print_uint128_t(n / 10);
+	putchar((n % 10) + '0');
+}
+
+/**
+ * print_space - prints a comma followed by a space
+ *
+ * Return: nothing
+ */
+void print_space(void)
+{
+	putchar(','), putchar(' ');
+}
 /**
  * main - prints first 50 Fibonacci numbers
  *
@@ -8,17 +31,21 @@
  */
 int main(void)
 {
-	long prev, curr, nxt, i;
+	__uint128_t prev, curr, nxt;
+	int i;
 
 	prev = 1, curr = 2, nxt = 0;
-	printf("%lu, %lu, ", prev, curr);
+	print_uint128_t(prev);
+	print_space();
+	print_uint128_t(curr);
+	print_space();
 	for (i = 1; i <= 96; i++)
 	{
 		nxt = prev + curr;
 		prev = curr, curr = nxt;
-		printf("%lu", curr);
+		print_uint128_t(curr);
 		if (i < 96)
-			printf(", ");
+			print_space();
 	}
 	putchar('\n');
 	return (0);

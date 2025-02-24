167352e0e7e8a40012d0f468c51c0c8e558b8527
marcolminc <thommarcolminc@gmail.com>
2025-02-24 12:23:23 -0800

    refactor by introducing _strlen helper function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,21 @@
 
 
 /**
+ * _strlen - returns the length of a string
+ * @s: the string to measure
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s && *s != '\0')
+		len++, s++;
+	return (len);
+}
+/**
  * print_rev - prints a string in reverse, followed by a new line
  * @s: the string
  *
@@ -11,12 +26,8 @@ void print_rev(char *s)
 {
 	int len;
 
-	len = 0;
-	while (s && *s != '\0')
-		len++, s++;
-	if (!len)
-		goto endl;
-	while (len-- >= 0)
-		_putchar(*s--);
-endl:	_putchar('\n');
+	len = _strlen(s) - 1;
+	while (len >= 0)
+		_putchar(*(s + len--));
+	_putchar('\n');
 }

2c9953277e6f72ae2dc5747577f3b9af0b59a545
marcolm <thommarcolminc@gmail.com>
2025-03-14 23:04:47 +0300

    I wonder what checker catches

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,13 @@
  * main - prints the number of args passed to it
  * @argc: the  number of args
  * @argv: the array of args
+ *
  * Return: exit status (0 for success)
  */
-int main(int argc, char **argv)
+int main(int argc, char argv[])
 {
-	(void)argv;
+	(void) argv;
 
-	printf("%d\n", argc - 1);
+	printf("%d\n", (argc - 1));
 	return (0);
 }

f0d7da57decc8db5f6d40daf568318801c7784b3
marcolm <thommarcolminc@gmail.com>
2025-03-14 12:20:13 +0300

    Write a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the number of CLI args
+ * @argc: number of CLI args
+ * @argv: vector of CLI args
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}

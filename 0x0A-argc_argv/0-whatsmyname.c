52f2d6b825390599ae08988a2f9a5d50fb8fa6a3
marcolm <thommarcolminc@gmail.com>
2025-03-14 12:15:41 +0300

    Write a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the name of the executable for this program
+ * @argc: number of CLI args
+ * @argv: vector of CLI args
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

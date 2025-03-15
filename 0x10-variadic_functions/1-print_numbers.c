e8982258eddb7cd1d7749cd81fed2a8ca617a202
marcolm <thommarcolminc@gmail.com>
2025-03-16 02:20:47 +0300

    Write a function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+
+/**
+ * print_numbers - prints numbers
+ * @separator: the number separator
+ * @n: number of numbers to print
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	if (n > 0)
+	{
+		va_start(ap, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (separator)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,5 +1,6 @@
 # 0x10 - Variadic Functions
 Table of Contents
 1. 0. Beauty is variable, ugliness is constant
+2. 1. To be is to be the value of a variable
 
 

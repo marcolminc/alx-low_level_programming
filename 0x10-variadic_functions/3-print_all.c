0eabeb484ceea662ce53fecd1b24155c3669264d
marcolm <thommarcolminc@gmail.com>
2025-03-16 16:26:08 +0300

    update the function
    remove redundant helper functions, increment i at the default case of switching format specifiers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,61 +3,6 @@
 
 
 /**
- * num_sep - gives number of specified formats from the format string
- * @str: the given format string
- *
- * Return: number of specified formats
- */
-int num_sep(const char * const str)
-{
-	int i, n;
-
-	i = n = 0;
-	while (str && str[i] != '\0')
-	{
-		switch (str[i])
-		{
-			case 'c':case 'i':case 'f':case 's':
-				n++;
-				break;
-			default:
-				break;
-		}
-		i++;
-	}
-	return (n);
-}
-
-/**
- * sep - prints out the separation chars (, )
- * @seps: remaining number of separators to print
- *
- * Return: nothing
- */
-void sep(int *seps)
-{
-	(*seps)--;
-	if (*seps > 0)
-		printf(", ");
-}
-
-/**
- * print_str - prints string for format 's'
- * @str: string to print, extracted from ap
- *
- * Return: nothing
- */
-void print_str(char *str)
-{
-	if (!str)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
-/**
  * print_all - prints the args as specified by format string
  * @format: the format string
  *
@@ -65,36 +10,37 @@ void print_str(char *str)
  */
 void print_all(const char * const format, ...)
 {
-	int i, seps;
+	int i; 
 	va_list ap;
-	char *str;
+	char *str, *nil;
 
+	nil = "(nil)";
 	va_start(ap, format);
-	seps = num_sep(format), i = 0;
-	while (format && format[i] != '\0')
+	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(ap, int));
-				sep(&seps);
 				break;
 			case 'i':
 				printf("%d", va_arg(ap, int));
-				sep(&seps);
 				break;
 			case 'f':
 				printf("%f", va_arg(ap, double));
-				sep(&seps);
 				break;
 			case 's':
 				str = va_arg(ap, char *);
-				print_str(str);
-				sep(&seps);
+				if (!str)
+					str = nil;
+				printf("%s", str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
+		if (format[i + 1])
+			printf(", ");
 		i++;
 	}
 }

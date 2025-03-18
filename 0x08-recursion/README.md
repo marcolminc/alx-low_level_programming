6980abd5adef3ced3774e0a683f6fe5e5b3777e7
marcolm <thommarcolminc@gmail.com>
2025-03-18 16:44:08 +0300

    Write a function that returns the length of a string

--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,4 +2,5 @@
 Table of contents:
 1. 0 - puts recursion
 2. 1 - print rev recursion
+3. 2 - strlen recursion
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
+int _strlen_recursion(char *s);
 
 
 #endif /* MAIN_H */

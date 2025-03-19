076e2002aacf2c67f90aafedcce18ba4393b0fbd
marcolm <thommarcolminc@gmail.com>
2025-03-19 22:43:17 +0300

    Define a new type struct dog with elements: dog's name, owner's name, dog's age

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 # 0x0E - Structures, Typedef
 Table of contents:
+1. 0 - Poppy
 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* structures */
+/**
+ * struct dog - represents dog ownership data (details)
+ * @name: name of a dog
+ * @owner: owner of a dog
+ * @age: age of a dog
+ *
+ * Description: This structure is an object for storing information
+ * about a dog and its ownership.
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+#endif /* DOG_H */

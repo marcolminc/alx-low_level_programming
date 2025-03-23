ca8fe99be5af35480466aa6cd4a30b74998bf9e7
marcolm <thommarcolminc@gmail.com>
2025-03-23 16:43:26 +0300

    refactor strchr to use recursion

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,23 +3,31 @@
 
 
 /**
- * _strchr - locates a character in a string
- * @s: the string
- * @c: the character to locate in s
+ * _strchr_helper - recursively finds c in s
+ * @s: given string
+ * @c: character to find
  *
- * Return: pointer to c in s when found, NULL otherwise
+ * Return: pointer to location of c in s
+ */
+char *_strchr_helper (char *s, char c)
+{
+	if (*s == c)
+		return (s);
+	if (!*s)
+		return (NULL);
+	return (_strchr_helper(++s, c));
+}
+
+
+/**
+ * _strchr - finds a character in a string
+ * @s: the given string
+ * @c: character to find in s
+ *
+ * Return: location of c in s, NULL otherwise
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
-	char *res;
-
-	res = NULL;
-	for (i = 0; s && *(s + i); i++)
-		if (*(s + i) == c)
-		{
-			res = (s + i);
-			break;
-		}
-	return (res);
+	return (_strchr_helper (s, c));
 }
+

8f9466a21e43a172b91362766b9c4cda2a96b79c
marcolm <thommarcolminc@gmail.com>
2025-03-23 15:18:56 +0300

    Remove 0x07 dir to begin afresh

--- 0x07-pointers_arrays_strings/README.md
+++
@@ -1,5 +0,0 @@
-# 0x07. C - Even More Pointers, Arrays and Strings
-Table of contents:
-1. 0 - memset
-2. 
-11. 101-crackme password
--- 0x07-pointers_arrays_strings/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/* includes */
-
-/* prototypes */
-char *_memset(char *s, char b, unsigned int n);
-
-#endif /* MAIN_H */

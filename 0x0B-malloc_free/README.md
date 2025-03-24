5f2db554cab59021e5865a5c3d4983d8e7f1cea3
marcolm <thommarcolminc@gmail.com>
2025-03-25 02:31:26 +0300

    Write a function that duplicates a string to another memory location

--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,5 +1,6 @@
 # 0x0B - Malloc, Free
 Table of contents:
 1. 0 - create array
+2. 1 - strdup
 
 
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,6 +9,7 @@
 /* prototypes */
 int _putchar(char);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 
 #endif /* MAIN_H */

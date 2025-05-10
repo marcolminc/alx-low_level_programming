17aa540345fec795dbe24b70aabffa921154abcc
marcolminc <thommarcolminc@gmail.com>
2025-05-10 14:51:48 +0300

    Check if string for new node is NULL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,13 +5,14 @@
 /**
  * _strlen - gives length of a string (number of characters)
  * @str: the string to measure
+ * @acc: accumulator for the length of str
  * Return: length of str
  */
 size_t _strlen(char *str, size_t acc)
 {
 	if (!str || *str == '\0')
 		return (acc);
-	return _strlen(str + 1, acc + 1);
+	return (_strlen(str + 1, acc + 1));
 }
 
 /**
@@ -23,12 +24,19 @@ size_t _strlen(char *str, size_t acc)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	char *node_str;
 
 	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = _strlen((char *)str, 0);
+	node_str = strdup(str);
+	if (!node_str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = node_str;
+	new->len = _strlen(node_str, 0);
 	new->next = *head;
 	*head = new;
 	return (*head);

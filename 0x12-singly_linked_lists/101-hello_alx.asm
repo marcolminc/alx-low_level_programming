87a54f16cdac703cd774aa28ce237530913d4718
marcolminc <thommarcolminc@gmail.com>
2025-05-11 05:19:31 +0300

    update assembly code

--- 0x12-singly_linked_lists/101-hello_alx.asm
+++ 0x12-singly_linked_lists/101-hello_alx.asm
@@ -1,14 +1,21 @@
 section .note.GNU-stack noalloc noexec nowrite progbits
 
+global main
+extern printf
+
 section .data
 	msg db "Hello, ALX", 10, 0	; 10 = \n, 0 = \0
 
 section .text
-extern printf	; declaring an external symbol
-global main	; C entry point (GCC)
-
 main:
-	mov rdi, msg	; 1st arg for printf - format string
-	xor rax, rax	; clear additional args - no specifiers
-	call printf	; finally invoke printf
+	push rbp
+	mov rbp, rsp
+
+	lea rdi, [msg]
+	xor rax, rax
+	call printf
+
+	mov rsp, rbp
+	pop rbp
+	xor rax, rax
 	ret

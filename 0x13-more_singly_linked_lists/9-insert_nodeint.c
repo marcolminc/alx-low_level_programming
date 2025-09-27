ca64b182bbc616082dbf99d683df8a7507b21ed1
marcolminc <thommarcolminc@gmail.com>
2025-09-28 01:58:44 +0300

    update implementation using prev and curr pointers

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(*new));
 	if (!new)
 		return (NULL);
-	new->n = n;
+	new->n = n, new->next = NULL;
 	if (!idx)
 		new->next = *head, *head = new;
 	prev = NULL, curr = *head;

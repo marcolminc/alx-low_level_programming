ce411d95c43690606700163c39600f69e01539ce
marcolminc <thommarcolminc@gmail.com>
2025-10-02 00:50:26 +0300

    write a function that prints a listint_t linked list safely (in case of cycles)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -84,7 +84,7 @@ bool is_hashed(hash_t *hash, listint_t *ptr)
 	for (i = 0; i < hash->pos; i++)
 		if (hash->hash[i] == ptr)
 		{
-			printf("-> %d\n", ptr->n);
+			printf("-> %d\n%d\n", ptr->n, (int)hash->pos);
 			return (true);
 		}
 	return (false);

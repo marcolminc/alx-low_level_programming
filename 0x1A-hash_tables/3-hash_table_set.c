12941396c09a9786ebea26f6b74c29e168b00319
marcolminc <thommarcolminc@gmail.com>
2025-10-08 15:42:23 +0300

    free value before replacing it in case of collision by the same key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(key, ptr->key) == 0)
 			{
+				free(ptr->value);
 				ptr->value = strdup(value);
 				return (1);
 			}

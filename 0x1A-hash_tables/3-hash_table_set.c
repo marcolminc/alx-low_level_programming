a621884dc08e24eea93f1a3e9bfe58da521f967e
marcolminc <thommarcolminc@gmail.com>
2025-10-08 04:25:48 +0300

    replace value in case of collision of both hash and key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,20 +15,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
 	unsigned long int idx;
+	hash_node_t *node, *ptr;
 
 	if (!ht || !strlen(key))
 		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx])
+	{
+		ptr = ht->array[idx];
+		while (ptr)
+		{
+			if (strcmp(key, ptr->key) == 0)
+			{
+				ptr->value = strdup(value);
+				return (1);
+			}
+			ptr = ptr->next;
+		}
+	}
 	node = malloc(sizeof(*node));
 	if (!node)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
-	idx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[idx] && ht->size == 1)
-		node->next = ht->array[idx];
+	node->next = ht->array[idx];
 	ht->array[idx] = node;
 	return (1);
 }

6ca953e5c06fb9433a97bf063d66050c30077b81
marcolminc <thommarcolminc@gmail.com>
2025-11-13 17:20:06 +0300

    use previous and step variables to go until exceeding size of array or finding the range within which the value is found

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,6 @@
 #include "search_algos.h"
-
+#include <math.h>
+#include <stdio.h>
 
 /**
  * jump_search - searches for a value in a sorted array of integers
@@ -15,28 +16,29 @@
  */
 int jump_search(const int *array, size_t size, int value)
 {
-	size_t a, b;
+	size_t step, prev;
 
-	a = 0;
-	b = (size_t)sqrt((double)size);
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	prev = 0;
 
 	do {
-		printf("Value checked array[%lu] = [%d]\n", a, array[a]);
-		if (b >= size || (int)b >= value)
-		{
-			printf("Value found between indexes [%lu] and [%lu]\n", a, b);
-			for (; a <= b && a < size; a++)
-			{
-				printf("Value checked array[%lu] = [%d]\n", a, array[a]);
-				if (array[a] == value)
-					return ((int)a);
-			}
-			break;
-		}
-		a = b;
-		b += (size_t)sqrt((double)size);
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		prev = step;
+		step += sqrt(size);
+	} while (prev < size && array[MIN(step, size) - 1] < value);
 
-	} while (array[MIN(b, size) - 1] < value);
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+
+	while (prev < MIN(step, size))
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return ((int)prev);
+		prev++;
+	}
+
 	return (-1);
 }
-

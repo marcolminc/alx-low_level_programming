8224963e4410e600edf4c67251df6843a9bdee59
marcolminc <thommarcolminc@gmail.com>
2026-01-11 14:14:14 +0300

    Write a function that searches for a value in a sorted list of integers using the jump search algorithm

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - searches for a value in a sorted list of integers using the jump search algorithm
+ * @list: a pointer to the head of the list to search in
+ * @size: the number of nodes in list
+ * @value: the value to search for
+ *
+ * Return: pointer to the first node where value is located, NULL otherwise
+ * - use square root of the size of the list as the jump step
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	listint_t *prev, *curr;
+	size_t step, step_size;
+
+	if (!list || !size)
+		return (NULL);
+	prev = curr = list;
+	step_size = (size_t)sqrt((double)size);
+
+	while (curr && curr->index + 1 < size && curr->n < value)
+	{
+		prev = curr;
+		for (step = 0; curr->next && step < step_size; step++)
+			curr = curr->next;
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		prev->index, curr->index);
+	for (; prev && prev->index <= curr->index; prev = prev->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		if (prev->index == curr->index)
+			break;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -49,3 +49,5 @@ int **allocate_map(int n, int m)
 9. 8
 10. 9.Interpolation search: Write a function that searches for a value in a sorted array of integers using the interpolation search algorithm
 11. 10.Exponential search: write a function that searches for a value in a sorted array of integers using the Exponential search algorithm
+12. 11.Advanced binary:
+13. 12.Jump search in a singly linked list
